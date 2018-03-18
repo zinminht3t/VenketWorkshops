c1f7b79a008ecfaebbab7f87fca605e156d8da33
Zin Min Htet <zinminht3t@gmail.com>
2018-03-18 15:43:32 +0800

    Movies CRUD

---
+++ MovieCRUD/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace MovieCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Movie ID : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Movie Title : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rental Price : ";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(344, 91);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(324, 31);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(344, 195);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(324, 31);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(344, 291);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(324, 31);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(193, 486);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(105, 42);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(344, 486);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(105, 42);
+            this.btnInsert.TabIndex = 7;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(482, 486);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(105, 42);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(619, 486);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(105, 42);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(129, 486);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(44, 42);
+            this.btnPrevious.TabIndex = 10;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(753, 486);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(44, 42);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(129, 561);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(64, 42);
+            this.btnFirst.TabIndex = 12;
+            this.btnFirst.Text = "<<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(722, 561);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 42);
+            this.btnLast.TabIndex = 13;
+            this.btnLast.Text = ">>";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(232, 561);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(303, 31);
+            this.txtFind.TabIndex = 14;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(582, 555);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(105, 42);
+            this.btnFind.TabIndex = 15;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 817);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Movies";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btnFind;
+    }
+}
+
---
+++ MovieCRUD/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieCRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn;
+        SqlCommand cm;
+        SqlDataAdapter da;
+        DataSet ds;
+        SqlCommandBuilder cmb;
+
+        int count = 0;
+        int lastrow = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string conS = "data source=(local);integrated security=SSPI;initial catalog=Dafesty";
+            cn = new SqlConnection(conS);
+            cm = new SqlCommand();
+            cm.CommandText = "Select videocode, movietitle, rentalprice from movies";
+            cm.Connection = cn;
+            da = new SqlDataAdapter(cm);
+            cmb = new SqlCommandBuilder(da);
+            ds = new DataSet();
+            da.Fill(ds, "Movies");
+            lastrow = ds.Tables[0].Rows.Count - 1;
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            count = lastrow;
+            FillTextBox();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FillTextBox();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if(count < lastrow)
+                count++;
+            FillTextBox();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (count == 0) { }
+            else
+            {
+                count--;
+            }
+            FillTextBox();
+        }
+        
+        private void FillTextBox()
+        {
+            txtID.Text = ds.Tables[0].Rows[count]["videocode"].ToString();
+            txtTitle.Text = ds.Tables[0].Rows[count]["movietitle"].ToString();
+            txtPrice.Text = ds.Tables[0].Rows[count]["rentalprice"].ToString();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            FillTextBox();
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ds.Tables[0].Rows[count]["videocode"] = txtID.Text;
+            ds.Tables[0].Rows[count]["movietitle"] = txtTitle.Text;
+            ds.Tables[0].Rows[count]["rentalprice"] = txtPrice.Text;
+            cmb.GetUpdateCommand();
+            da.Update(ds, "Movies");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            DataRow r = ds.Tables[0].NewRow();
+            r["videocode"] = txtID.Text;
+            r["movietitle"] = txtTitle.Text;
+            r["rentalprice"] = txtPrice.Text;
+            ds.Tables[0].Rows.Add(r);
+            da.Update(ds, "Movies");
+            lastrow++;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DataRow r = ds.Tables[0].Rows[count];
+            r.Delete();
+            da.Update(ds, "Movies");
+            lastrow--;
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            DataTable table = ds.Tables["Movies"];
+            table.PrimaryKey = new DataColumn[] { table.Columns["VideoCode"] };
+            DataRow r = table.Rows.Find(txtFind.Text);
+            if (r == null)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                txtID.Text = r[0].ToString();
+                txtTitle.Text = r[1].ToString();
+                txtPrice.Text = r[2].ToString();
+            }
+
+        }
+    }
+}
